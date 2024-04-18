170696ba0b0a056e9cf0494d422cb8d20e17c150
Dariusz Okoński <darek200180@gmail.com>
2024-04-18 21:00:07 +0200

    add nanoid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react';
 import data from './data';
+import { nanoid } from 'nanoid';
+
+const shuffle = (array) => {
+  let currentIndex = array.length;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+};
 
 const App = () => {
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    const localData = [...data];
+    shuffle(localData);
+    setText(localData.slice(0, amount));
+  };
+
   return (
     <section className='section-center'>
       <h4>Tired of boring lorem ipsum?</h4>
-      <form className='lorem-form'>
+      <form className='lorem-form' onSubmit={handleSubmit}>
         <label htmlFor='amount'>paragraphs:</label>
         <input
           type='number'
@@ -24,6 +50,12 @@ const App = () => {
           generate
         </button>
       </form>
+
+      <article className='lorem-text'>
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
     </section>
   );
 };
